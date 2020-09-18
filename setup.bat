82b75ec87c4a0995260ff6f85856dfd33c39bc43
robriu <ro.briuv@gmail.com>
2020-09-18 11:21:55 +0200

    dev/generate cpp proto classes

--- setup.bat
+++ setup.bat
@@ -1,4 +1,9 @@
+mkdir cpp\pb2_gen
 mkdir python\pb2_gen
+
+protoc -I=%CD%\proto --cpp_out=%CD%\cpp\pb2_gen %CD%\proto\print_settings.proto
+protoc -I=%CD%\proto --cpp_out=%CD%\cpp\pb2_gen %CD%\proto\request.proto
+protoc -I=%CD%\proto --cpp_out=%CD%\cpp\pb2_gen %CD%\proto\response.proto
 
 protoc -I=%CD%\proto --python_out=%CD%\python\pb2_gen %CD%\proto\print_settings.proto
 protoc -I=%CD%\proto --python_out=%CD%\python\pb2_gen %CD%\proto\request.proto
