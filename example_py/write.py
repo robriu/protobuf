283b0d9b3505c007afc6eae0ee36581c3d2698bb
robriu <ro.briuv@gmail.com>
2020-11-12 22:12:37 +0100

    dev/add python write example

--- example_py/write.py
+++ example_py/write.py
@@ -1,4 +1,5 @@
 import sys
+import utils_file as uf
 
 from pb2_gen import request_pb2
 from pb2_gen import settings_pb2
@@ -7,17 +8,17 @@ from pb2_gen import settings_pb2
 def ExampleWrite():
     req = CreateRequest()
 
-    print("-----------")
-    print(req)
+    if uf.VerifyRequestMessage(req):
+        uf.WriteRequestToFile(req, input("[INPUT] Enter output filename: "))
 
 ### Util Functions
 def CreateRequest():
     req = request_pb2.Request()
-    
+
     req.id = 1
     req.sequence = 1
     req.request_message = "Quality Enhancement Request"   # Optional field
-    
+
     settings = CreateSettings()
     # req_settings = req.set_request.set_settings
     req.set_request.set_settings.MergeFrom(settings)
@@ -28,10 +29,9 @@ def CreateSettings():
     set_settings = request_pb2.Request().set_request.set_settings
     set_settings.profile_id = int(input("Enter profile id: "))
     set_settings.requirements = input("Enter requirements: ") if input("Wish to add requirements? (y/n): ") == 'y' else 'Empty'
+    set_settings.print_settings.quiality.layer_height = 1.5
 
     return set_settings
-
-
 
 # Main
 ExampleWrite()
